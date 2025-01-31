d4c2cc92c43aa0ced9ddf8127be8de987936849c
saivasanth04 <143429518+saivasanth04@users.noreply.github.com>
2025-01-31 14:48:18 +0530

    Add files via upload
    this is sample

---
+++ 31-01-2025/31-01-2025.js
@@ -0,0 +1,9 @@
+import vasanth from "./sample.js"
+import {x,user,isers} from "./sample.js"
+
+let value=vasanth.name
+console.log(x)
+console.log(user)
+isers.map((user)=>{
+    console.log(user);
+})
